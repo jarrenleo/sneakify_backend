eb13330f168129004f312e663ea8d850fa106b92
Jarren Leo <jarren.leoo@gmail.com>
2024-02-07 17:50:50 +0800

    Deprecated StockX due to bot protection

--- routes/resaleRoute.js
+++ routes/resaleRoute.js
@@ -1,10 +1,8 @@
 import express from "express";
-import StockXPrice from "../services/stockx/priceData.js";
 import GoatPrice from "../services/goat/priceData.js";
 import SNKRDunkPrice from "../services/snkrdunk/priceData.js";
 import { locales } from "../utilities/settings.js";
 
-const stockx = new StockXPrice();
 const goat = new GoatPrice();
 const snkrdunk = new SNKRDunkPrice();
 
@@ -18,14 +16,11 @@ router.get("/", async (request, response, next) => {
       throw new Error("Missing required query parameters");
     if (!locales[country]) throw new Error("Country not supported");
 
-    const [stockxPrices, goatPrices, snkrdunkPrices] = await Promise.allSettled(
-      [
-        stockx.getPrices(sku, size, country),
-        goat.getPrices(sku, size, country),
-        snkrdunk.getPrices(sku, size, country),
-      ]
-    );
-    const data = [stockxPrices.value, goatPrices.value, snkrdunkPrices.value];
+    const [goatPrices, snkrdunkPrices] = await Promise.allSettled([
+      goat.getPrices(sku, size, country),
+      snkrdunk.getPrices(sku, size, country),
+    ]);
+    const data = [goatPrices.value, snkrdunkPrices.value];
 
     response.status(200).send(data);
   } catch (error) {
